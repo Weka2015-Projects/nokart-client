80cc21804a5725b98d22565b2af6f1ccef435c6c
NiluK <nkulasingham@gmail.com>
2016-02-08 16:51:50 +1300

    all tests passing

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,18 +1,30 @@
 const initialState = {
   products: [],
-  cart: {user: null, item: null}
+  cart: {user: {}, item: {}}
 }
 
 function reducer (state = initialState, action) {
   switch (action.type) {
     case 'ADD_USER': 
-      return state
+      var newState = state 
+      newState.cart.user = action.data
+      return newState
     case 'ADD_PRODUCT':
-      return state
+      var newState = state
+      var newData = action.data.map((item) => {
+        if(item.id && item.price && item.name) {
+          newState.products.push(item)
+        }
+      })
+      return newState
     case 'SELECT_PRODUCT':
-      return state
+      var newState = state
+      newState.cart.item = action.data
+      return newState
     case 'BUY_PRODUCT': 
-      return state
+      var newState = state
+      newState.cart.item = {}
+      return newState
   }
   return state
 }
